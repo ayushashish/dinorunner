0eb6593426306ed5c3e7298134c7f0971de6a12e
ayushashish <37033310+ayushashish@users.noreply.github.com>
2019-02-06 01:26:29 +0530

    Update dinorun.py

--- dinorun.py
+++ dinorun.py
@@ -3,29 +3,20 @@ import pyautogui
 import time
 from numpy import *
 print ("working...")
-flag=1
+
 def imageGrab():
     box=(332,408,397,452)
     image= ImageGrab.grab(box)
     grayImage = ImageOps.grayscale(image)
     a=array (grayImage.getcolors())
     print (a.sum())
+	#the actual value can be checked here
     return (a.sum())
 
 def main():
-    #flag=1
     while True:
         z=(imageGrab())
-        """if (z==3115):
-            flag=0
-        if (z==3115):
-            flag=1"""
-        if(z!=3115):
+        if(z!=3115):#or the value obtained above
             pyautogui.keyDown('up')
-            #time.sleep(0.000001)
-            #print ((imageGrab()))
-            #pyautogui.keyUp ('up')
-            #time.sleep(0.0000001)
-            #pyautogui.keyDown('up')
-            #pyautogui.keyUp('up')
 main ()
+
